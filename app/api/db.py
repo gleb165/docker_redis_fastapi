be723730edd23d25c23b69bd839148df28d906ae
gleb165 <83755952+gleb165@users.noreply.github.com>
2023-12-12 15:40:14 +0200

    add conect

--- app/api/db.py
+++ app/api/db.py
@@ -3,18 +3,20 @@ from sqlalchemy import (Column, Integer, MetaData, String, Table,
 
 from databases import Database
 
-DATABASE_URL = 'jdbc:postgresql://localhost:5432/postgres'
+DATABASE_URL = 'postgresql://postgres:пдуи@localhost/postgres'
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
 
-movies = Table(
-    'movies',
+performances = Table(
+    'performances',
     metadata,
     Column('id', Integer, primary_key=True),
     Column('name', String(50)),
+    Column('price', Integer),
     Column('plot', String(250)),
     Column('genres', ARRAY(String)),
     Column('description', String(250))
 )
+
 
 database = Database(DATABASE_URL)
