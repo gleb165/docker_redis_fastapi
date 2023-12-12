d8ced4d62701d31503d1055bc06b1b293813efff
gleb165 <83755952+gleb165@users.noreply.github.com>
2023-12-12 20:38:28 +0200

    dd

--- app/api/main.py
+++ app/api/main.py
@@ -1,20 +1,42 @@
-from fastapi import FastAPI
-from app.api.models import Performances
-from app.api.db import metadata, database, engine
-
-metadata.create_all(engine)
-
-app = FastAPI()
+from fastapi import APIRouter, Header, HTTPException
+from app.api.models import PerformancesIn, PerformancesUpdate, PerformancesOut
+from app.api import db_maneger
 
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
+per = APIRouter()
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+@per.get('/', response_model=PerformancesOut)
+async def get_all_():
+    return db_maneger.get_all_performance()
 
 
-app.include_router(Performances)
+@per.post('/', status_code=201)
+async def add(performances: PerformancesIn):
+    per_id = await db_maneger.add_performance(performances)
+    response = {
+        'id': per_id,
+        **performances.dict()
+    }
+    return response
+
+
+@per.put('/{id}')
+async def update(id: int, performances: PerformancesIn):
+    perfo = await db_maneger.get_performance(id)
+    if not perfo:
+        raise HTTPException(status_code=404, detail="not found")
+    update_data = performances.dict(exclude_unset=True)
+    per_in_db = PerformancesIn(**perfo)
+
+    updated_perfo = per_in_db.copy(update=update_data)
+
+    return await db_maneger.update_movie(id, updated_perfo)
+
+
+@per.delete('/{id}')
+async def delete(id: int, performances: PerformancesIn):
+    perfo = await db_maneger.get_performance(id)
+    if not perfo:
+        raise HTTPException(status_code=404, detail="not found")
+    return await db_maneger.delete_performance(id)
